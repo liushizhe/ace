eb68da7ac88657086e8cb6c5eee4706347d7c47c
liushizhe <ljs19870826@163.com>
2018-02-05 14:32:20 +0800

    add mouse enter and leave operation,add close button event,add fade in and fade out (fade out time can be set)

--- jGritter/jGritter.js
+++ jGritter/jGritter.js
@@ -1,8 +1,8 @@
 /*
 * @Author: linjs
 * @Date:   2018-02-03 10:50:01
-* @Last Modified by:   linjs
-* @Last Modified time: 2018-02-03 15:22:08
+* @Last Modified by:   liushizhe
+* @Last Modified time: 2018-02-05 14:20:18
 */
 (function($){
 	$.jGritter = {};
@@ -16,7 +16,7 @@
 	}
 
 	var jGritter = {
-		tpl: '<div class="jGritter [[class]]">'+
+		tpl: '<div class="jGritter [[class]]" style="display:none;">'+
 				'<div class="close">x</div>'+
 				'<div class="title">'+
 					'<p>'+
@@ -29,6 +29,7 @@
 					'</p>'+
 				'</div>'+
 			'</div>',
+		timeOutId: 0,
 		add: function(param) {
 			if (typeof param === 'string') {
 				param['text'] = param;
@@ -37,27 +38,70 @@
 				throw 'You must supply "text" parameter!';
 			}
 			var opt = {
-				time: 1000
-			};
-			param = $.extend(param, opt);
+		        time: 1000,
+		        title: '',
+		        text:   '',
+		        class_name: '',
+		        fadeOut: true
+		    };
+			param = $.extend(opt, param);
 			var class_name = param.class_name;
 			var title = param.title || '';
 			var text = param.text;
 			var time = param.time;
+			var fadeOut = param.fadeOut;
+			var sticky = param.sticky;
 			var tmp = this.tpl;
 			tmp = tmp.replace(/\[\[class\]\]/, class_name);
 			tmp = tmp.replace(/\[\[title\]\]/, title);
 			tmp = tmp.replace(/\[\[content\]\]/, text);
 			$('body').append(tmp);
-			var _this = this;
-			setTimeout(function() {
-				_this.remove();
-			}, time);
+			$('body .jGritter').fadeIn(500);
+			if(!sticky) {
+				var _this = this;
+				var e = $('body .jGritter');
+				this.setFadeOut(e, fadeOut, time);
+				//when mouse enter, the gritter keep stick;mouse leave, disappear
+				e.bind('mouseenter mouseleave', function(event){
+					if(event.type == 'mouseenter'){
+						_this.resetFadeOut();
+					}
+					else {
+						_this.setFadeOut(e, fadeOut, 0);
+					}
+				});
+				//gritter close button
+				e.find('.close').click(function(event) {
+					_this.setFadeOut(e, fadeOut, 0);
+				});
+			}
 		},
 		remove: function() {
 			if($('body .jGritter')[0] !== undefined) {
-				// $('body .jGritter').remove();
+				$('body .jGritter').remove();
 			}
+		},
+		setFadeOut: function(e, fadeOut, time) {
+			var _this = this;
+			this.timeOutId = setTimeout(function() {
+				if(fadeOut === true){
+					fadeOut = 1000;
+				}
+				if(fadeOut !== 0) {
+					e.animate({
+						opacity: 0
+					}, fadeOut, function(){
+						e.animate({ height: 0 }, 300, function(){
+							_this.remove();
+						})
+					})
+				} else {
+					_this.remove();
+				}
+			}, time);
+		},
+		resetFadeOut: function() {
+			clearTimeout(this.timeOutId);
 		}
 	}
 })(jQuery);
